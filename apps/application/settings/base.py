20d3b2c794336e297bf20a10aadac19724d2dca4
James Canning <james@jamescanning.com>
2017-04-15 17:31:10 +0100

    🥂 Fix assets path name

--- apps/application/settings/base.py
+++ apps/application/settings/base.py
@@ -116,12 +116,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 AUTH_USER_MODEL = 'falmer_auth.FalmerUser'
