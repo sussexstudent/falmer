0c3b0dfb95732a3e03fb4a591766810df3f7cc4d
James Canning <james@jamescanning.com>
2017-04-21 22:05:19 +0100

    ☁️ Use CF for media

--- apps/application/settings/production.py
+++ apps/application/settings/production.py
@@ -17,7 +17,7 @@ X_FRAME_OPTIONS = 'DENY'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('MEDIA_HOST')
 MEDIA_URL = "https://%s/" % os.environ.get('MEDIA_HOST')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
