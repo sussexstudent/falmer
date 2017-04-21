0d6faa7193cbfd3f5a478a79afa8fda0f47c8d9f
James Canning <james@jamescanning.com>
2017-04-21 16:33:43 +0100

    📦 Add django-storages for media on S3

--- apps/application/settings/production.py
+++ apps/application/settings/production.py
@@ -14,8 +14,16 @@ CSRF_COOKIE_HTTPONLY = True
 # SECURE_HSTS_SECONDS = 3600 - not yet
 X_FRAME_OPTIONS = 'DENY'
 
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
+    'storages',
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
