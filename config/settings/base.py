74c037a80ac443057c42b7b41ac6570b72d9da69
James Canning <james@jamescanning.com>
2017-05-29 17:32:00 +0100

    Remove allauth

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,9 +45,6 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     'crispy_forms',  # Form layouts
-    'allauth',  # registration
-    'allauth.account',  # registration
-    'allauth.socialaccount',  # registration
 
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
@@ -265,17 +262,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # ------------------------------------------------------------------------------
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-# Some really nice defaults
-ACCOUNT_AUTHENTICATION_METHOD = 'username'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-
-ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
-ACCOUNT_ADAPTER = 'falmer.users.adapters.AccountAdapter'
-SOCIALACCOUNT_ADAPTER = 'falmer.users.adapters.SocialAccountAdapter'
 
 # Custom user app defaults
 # Select the correct user model
