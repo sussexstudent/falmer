8ede2a7e7be73b08e78285e463d60a50363a7e8f
James Canning <james@brudil.com>
2017-06-11 11:37:50 +0100

    Add Matte models to admin

--- falmer/matte/admin.py
+++ falmer/matte/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from .models import MatteImage, MatteRendition
+
+
+@register(MatteImage)
+class MatteImageModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@register(MatteRendition)
+class MatteRenditionModelAdmin(admin.ModelAdmin):
+    pass
