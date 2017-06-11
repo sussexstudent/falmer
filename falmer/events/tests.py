840ce541f4a80e786325f7d9e4ac9583528b5ea0
James Canning <james@brudil.com>
2017-06-11 18:58:57 +0100

    Fix event test

--- falmer/events/tests.py
+++ falmer/events/tests.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.test import TestCase
 
 from .utils import parse_date
@@ -8,7 +10,7 @@ class DateParsingTest(TestCase):
     def test_spanning_dates(self):
         parsed = parse_date('29th May 9am - 11th June 5pm')
         self.assertEqual(parsed, {
-            'start_date': '',
-            'end_date': '',
-            'is_multi_day': True,
+            'start_date': datetime.datetime(2017, 5, 29, 9, 0),
+            'end_date': datetime.datetime(2017, 6, 11, 17, 0),
+            'is_over_multiple_days': True,
         })
