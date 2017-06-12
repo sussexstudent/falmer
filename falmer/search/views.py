b3640b09eabd3cf30eab9d9e16ad9d5c01396674
James Canning <james@brudil.com>
2017-06-12 12:07:36 +0100

    Cache search results for an hour

--- falmer/search/views.py
+++ falmer/search/views.py
@@ -1,7 +1,22 @@
+from django.core.cache import cache
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
+
 from .utils import get_results_for_term
 
+@api_view(['GET'])
 def search(request):
-    data = get_results_for_term(request.GET['q'])
+    search_term = request.GET['q']
+    cache_key = 'sr_{}'.format(search_term)
 
-    return JsonResponse({'results': data})
+    cached = cache.get(cache_key)
+
+    if cached is not None:
+        return Response(cached)
+
+    data = get_results_for_term(search_term)
+
+    cache.set(cache_key, data, 60 * 60)
+
+    return Response(data)
