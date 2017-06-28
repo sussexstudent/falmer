5b616f5b3d605b1831d4ca8ca0a9be561f399a89
James Canning <james@brudil.com>
2017-06-28 11:20:25 +0100

    Improve list display of events

--- falmer/events/admin.py
+++ falmer/events/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from falmer.events.models import Event
+from falmer.events.models import Event, MSLEvent
 
 
 @register(Event)
 class EventModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_time', 'end_time', )
+
+@register(MSLEvent)
+class MSLEventModelAdmin(admin.ModelAdmin):
     pass
