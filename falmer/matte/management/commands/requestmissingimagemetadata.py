9b2951e212ec37fa1a7482c6359f2b973bc6da59
James Canning <james@brudil.com>
2017-09-13 14:08:30 +0100

    Look for lack of em rather than initial

--- falmer/matte/management/commands/requestmissingimagemetadata.py
+++ falmer/matte/management/commands/requestmissingimagemetadata.py
@@ -4,7 +4,8 @@ from ...tasks import perform_external_image_analysis
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        images = MatteImage.objects.filter(external_metadata__last_label_request_status='NA')
+        images = MatteImage.objects.filter(external_metadata=None)
 
         for image in images:
+            print('queuing {}'.format(image.pk))
             perform_external_image_analysis.delay(image.pk)
