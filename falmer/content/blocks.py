f28472973e1d1c445c2a5f96615b9b6b86bbce01
James Canning <james@brudil.com>
2017-11-01 13:18:29 +0000

    Expand api content

--- falmer/content/blocks.py
+++ falmer/content/blocks.py
@@ -1,4 +1,6 @@
 from wagtail.wagtailcore import blocks
+from wagtail.wagtailcore.blocks import RichTextBlock
+from wagtail.wagtailcore.rich_text import expand_db_html
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 from falmer.content.serializers import WagtailImageSerializer
@@ -7,6 +9,11 @@ from falmer.content.serializers import WagtailImageSerializer
 class ImageBlock(ImageChooserBlock):
     def get_api_representation(self, value, context=None):
         return WagtailImageSerializer(value).data
+
+
+class RichTextWithExpandedContent(RichTextBlock):
+    def get_prep_value(self, value):
+        return expand_db_html(value.source)
 
 
 class ContactBlock(blocks.StructBlock):
@@ -21,7 +28,7 @@ class ContactBlock(blocks.StructBlock):
 class SectionBlock(blocks.StructBlock):
     heading = blocks.CharBlock(required=True)
     body = blocks.StreamBlock([
-        ('paragraph', blocks.RichTextBlock()),
+        ('paragraph', RichTextWithExpandedContent()),
     ])
 
     class Meta:
@@ -40,7 +47,7 @@ class FigureBlock(blocks.StructBlock):
 
 class PledgeBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=True)
-    body = blocks.RichTextBlock(required=True)
+    body = RichTextWithExpandedContent(required=True)
     image = ImageBlock()
     status = blocks.ChoiceBlock(choices=[
         ('in_progress', 'In Progress'),
