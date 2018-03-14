f3b062bc72b5712aa3bb25001496c4f63c3b029b
James Canning <james@brudil.com>
2018-03-14 14:38:25 +0000

    Fix magic link url

--- falmer/auth/urls.py
+++ falmer/auth/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('logout/', logout_view, name='auth-logout'),
     path('login-request/', email_link_request, name='auth-request'),
     path('login-sent/', email_link_sent, name='auth-request-sent'),
-    path('magic-link/<int:token>/', magic_link_login, name='auth-magic_link'),
+    path('magic-link/<slug:token>/', magic_link_login, name='auth-magic_link'),
     path('token/', generate_jwt, name='auth-magic_link'),
     path('me/', me, name='auth-magic_link'),
 ]
