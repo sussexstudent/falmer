e6a1db7a6840e899aa5b30177bc2bddbcd50f031
James Canning <james@brudil.com>
2018-03-21 11:28:59 +0000

    fix path arg

--- falmer/frontend/views.py
+++ falmer/frontend/views.py
@@ -24,7 +24,7 @@ dev = """
 """
 
 
-def application_serve(request, path):
+def application_serve(request):
     if request.is_ajax() is False:
         try:
             deployment = FrontendDeployment.objects.filter(enabled=True).latest('created_at')
