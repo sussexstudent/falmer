0d30574b190d6af5abc95dce9fedf652a96f5f2e
James Canning <james@brudil.com>
2018-04-18 12:29:14 +0100

    Add content/main to offers

--- falmer/commercial/types.py
+++ falmer/commercial/types.py
@@ -1,3 +1,4 @@
+from graphene.types.generic import GenericScalar
 from graphene_django import DjangoObjectType
 from . import models
 
@@ -5,3 +6,8 @@ from . import models
 class Offer(DjangoObjectType):
     class Meta:
         model = models.Offer
+
+    main = GenericScalar()
+
+    def resolve_main(self, info):
+        return self.main.stream_block.get_api_representation(self.main, info.context)
