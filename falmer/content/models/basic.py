6ae8814570004bb7fa6f542b1769c61c5d9b7a87
James Canning <james@brudil.com>
2018-08-15 15:41:21 +0100

    Add content to BasicContentPage

--- falmer/content/models/basic.py
+++ falmer/content/models/basic.py
@@ -1,3 +1,8 @@
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.blocks import StreamBlock
+from wagtail.core.fields import StreamField
+
+from falmer.content import components
 from falmer.content.models.core import Page
 
 
@@ -6,4 +11,19 @@ class StubPage(Page):
 
 
 class BasicContentPage(Page):
-    pass
+    content = StreamField(
+        StreamBlock([
+            components.text.to_pair(),
+            components.callout.to_pair(),
+            components.alert.to_pair(),
+            components.inset.to_pair(),
+            components.image.to_pair(),
+            components.button_group_links.to_pair(),
+        ]), verbose_name='Main Content',
+        null=True, blank=True
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content'),
+    ]
+
