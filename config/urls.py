5f703c5862ac42bb9fc0fed9028724fc9f7b0b0a
James Canning <james@brudil.com>
2018-10-04 13:39:06 +0100

    Fix page resolver

--- config/urls.py
+++ config/urls.py
@@ -63,6 +63,7 @@ urlpatterns = [
                   path('', include(launcher_urls)),
                   path('', include(wagtail_urls)),
                   path('', include(frontend_urls)),
+                  path('', include(wagtail_urls)),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
