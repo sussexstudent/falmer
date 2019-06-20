0edc91468c5f424a57be80675422723f9bac4a89
James Canning <james@brudil.com>
2019-06-20 12:49:18 +0100

    Replace FalmerUserAdmin with extention of base

--- falmer/auth/admin.py
+++ falmer/auth/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from django.contrib.admin import register
-from . import models
+from django.contrib.auth.admin import UserAdmin
 
-@register(models.FalmerUser)
-class FalmerUserModelAdmin(admin.ModelAdmin):
+# @register(models.FalmerUser)
+# class FalmerUserModelAdmin(admin.ModelAdmin):
+#     list_display = ('name_or_email', 'identifier', 'authority')
+#     list_filter = ('authority', )
+#     search_fields = ('name', 'identifier')
+from falmer.auth import models
+
+
+class FalmerUserAdmin(UserAdmin):
+    ordering = ('name', 'identifier')
     list_display = ('name_or_email', 'identifier', 'authority')
-    list_filter = ('authority', )
-    search_fields = ('name', 'identifier')
 
+    fieldsets = (
+        (None, {'fields': ('identifier', 'authority')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+                                     })
+    )
+
+
+admin.site.register(models.FalmerUser, FalmerUserAdmin)
